1adf34f6fd13fb99d72222e2d12dae34aec6d840
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-18 16:03:43 -0400

    Create and delete a post

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,12 +8,12 @@ const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
 
-//  @route GET api/autg
+//  @route GET api/auth
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password')
     res.json(user)
-  } catch(err) {
+  } catch (err) {
     console.error(err.message)
     res.status(500).send('Server error')
   }
@@ -23,39 +23,39 @@ router.get('/', auth, async (req, res) => {
 // Authenticate user and get token
 
 router.post(
-  '/', 
+  '/',
   [
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Password is required').exists()
-  ], 
-    async (req, res) => {
+  ],
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
     const { email, password } = req.body
-  
+
     try {
       let user = await User.findOne({ email })
-  
+
       if (!user) {
-         return res.status(400).json({ errors: [ { msg: 'Invalid Credentials' }] })
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] })
       }
 
       const isMatch = await bcrypt.compare(password, user.password)
 
       if (!isMatch) {
-        return res.status(400).json({ errors: [ { msg: 'Invalid Credentials' }] })
-     }
-      
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] })
+      }
+
       const payload = {
         user: {
           id: user.id
         }
       }
-  
+
       jwt.sign(
-        payload, 
+        payload,
         config.get('jwtSecret'),
         { expiresIn: 360000 },
         (err, token) => {
@@ -63,8 +63,8 @@ router.post(
           res.json({ token })
         }
       )
-  
-    } catch(err) {
+
+    } catch (err) {
       console.error(err.message)
       res.status(500).send('Server error')
     }
