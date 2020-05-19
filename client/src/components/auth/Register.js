33233d999b25281d7e4776aac244038fdbb8ed8b
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-19 13:04:03 -0400

    Revert "Finished Login and Register forms"
    This reverts commit 77d0c48998a43f45179814579ab471d67284c331.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useState } from 'react'
-import { Link } from 'react-router-dom'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -13,12 +12,12 @@ const Register = () => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  const onSubmit = async e => {
+  const onSubmit = e => {
     e.preventDefault()
     if (password !== password2) {
       console.log('Passwords do not match')
     } else {
-      console.log('Success')
+      console.log(formData)
     }
   }
 
@@ -72,7 +71,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <Link to="/login">Sign In</Link>
+        Already have an account? <a href="login.html">Sign In</a>
       </p>
     </Fragment>
   )
