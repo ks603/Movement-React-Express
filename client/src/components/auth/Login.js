d613cd15d22c6d7bc017822c0a649481aebffbb1
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-19 13:47:52 -0400

    Created Redux store

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    password: '',
-    password2: ''
+    email: '',
+    password: ''
   })
 
   const { email, password } = formData
