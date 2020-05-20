9d92fddbe691d8c47acbcd2faf3249beac2c9118
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-20 03:27:15 -0400

    Can now register and login in with authentication

--- client/src/store.js
+++ client/src/store.js
@@ -1,3 +1,4 @@
+
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
@@ -17,17 +18,16 @@ const store = createStore(
 // set up a store subscription listener
 // to store the users token in localStorage
 
-// prevent auth error on first run of subscription
-let currentState = {
-  auth: { token: null, isAuthenticated: null, loading: true, user: null }
-};
+// initialize current state from redux store for subscription comparison
+// preventing undefined error
+let currentState = store.getState();
 
 store.subscribe(() => {
   // keep track of the previous and current state to compare changes
   let previousState = currentState;
   currentState = store.getState();
   // if the token changes set the value in localStorage and axios headers
-  while (previousState.auth.token !== currentState.auth.token) {
+  if (previousState.auth.token !== currentState.auth.token) {
     const token = currentState.auth.token;
     setAuthToken(token);
   }
