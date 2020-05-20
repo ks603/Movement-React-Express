8770c45c5f1bcd6515cee10f7c0b042349efd14e
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-20 00:39:45 -0400

    Trying to get Login and Register to work

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,9 +4,12 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from './types'
 import setAuthToken from '../utils/setAuthToken'
+
 
 // Load User
 export const loadUser = () => async dispatch => {
@@ -16,7 +19,6 @@ export const loadUser = () => async dispatch => {
 
   try {
     const res = await axios.get('/api/auth')
-
     dispatch({
       type: USER_LOADED,
       payload: res.data
@@ -27,7 +29,6 @@ export const loadUser = () => async dispatch => {
     })
   }
 }
-
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
@@ -47,6 +48,7 @@ export const register = ({ name, email, password }) => async dispatch => {
       payload: res.data
     })
 
+    dispatch(loadUser())
   } catch (err) {
     const errors = err.response.data.errors
 
@@ -60,3 +62,29 @@ export const register = ({ name, email, password }) => async dispatch => {
   }
 }
 
+// Login User
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  const body = JSON.stringify({ email, password })
+  try {
+    const res = await axios.post('/api/auth', body, config)
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    })
+    dispatch(loadUser())
+  } catch (err) {
+    const errors = err.response.data.errors
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
+    })
+  }
+}
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,8 +1,12 @@
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL,
+  //REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  //LOGIN_FAIL,
+  LOGOUT,
+  ACCOUNT_DELETED
 } from '../actions/types';
 
 const initialState = {
@@ -22,23 +26,37 @@ export default function (state = initialState, action) {
         isAuthenticated: true,
         loading: false,
         user: payload
-      }
+      };
     case REGISTER_SUCCESS:
-      localStorage.setItem('token', payload.token);
       return {
         ...state,
         ...payload,
         isAuthenticated: true,
         loading: false
       };
-    case REGISTER_FAIL:
-    case AUTH_ERROR:
-      localStorage.removeItem('token');
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case ACCOUNT_DELETED:
       return {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: false
+        loading: false,
+        user: null
+      };
+    case AUTH_ERROR:
+    case LOGOUT:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null
       };
     default:
       return state;
