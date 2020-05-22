150d2939a638802201f97babcbc33e611ee21304
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-22 17:07:29 -0400

    Profile now populates the dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import DashboardActions from './DashboardActions'
+import Experience from './Experience'
 import { getCurrentProfile } from '../../actions/profile'
 
 const Dashboard = ({
@@ -19,7 +20,7 @@ const Dashboard = ({
     <p className='lead'>
       <i className='fas fa-user' /> Welcome {user && user.name}
     </p>
-    {profile !== null ? <Fragment> <DashboardActions /></Fragment> : <Fragment><p>You have not yet setup a profile, please add some info!</p>
+    {profile !== null ? <Fragment> <DashboardActions /> <Experience experience={profile.experience} /></Fragment> : <Fragment><p>You have not yet setup a profile, please add some info!</p>
       <Link to='/create-profile' className="btn btn-primary my-1">
         Create Profile
       </Link>
