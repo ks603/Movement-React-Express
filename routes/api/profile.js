8d2fffe1565a0f322195b677e7c841be1ded1e72
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-23 17:18:49 -0400

    Gravatar typo fixed and about section created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -112,7 +112,7 @@ router.post(
 
 router.get('/', async (req, res) => {
   try {
-    const profiles = await Profile.find().populate('user', ['name', 'avater'])
+    const profiles = await Profile.find().populate('user', ['name', 'avatar'])
     res.json(profiles)
   } catch (err) {
     console.error(err.message)
