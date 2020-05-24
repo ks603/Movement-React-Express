cc3e18ed3246ff54a73933a109defa08083e8fa3
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-23 20:25:09 -0400

    Created post and comment

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
+import post from './Post'
 
 export default combineReducers({
   alert,
   auth,
-  profile
+  profile,
+  post
 });
